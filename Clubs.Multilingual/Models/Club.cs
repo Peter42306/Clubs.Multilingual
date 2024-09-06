4a06729e3457c2b4a6ece0bbeba2e8b7670f623e
Petr Zalizko <pzalizko@gmail.com>
2024-09-06 21:50:40 +0300

    App not runs. Resources files...

---
+++ Clubs.Multilingual/Models/Club.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Resources;
+
+namespace Clubs.Multilingual.Models
+{
+    public class Club
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Resources.Resource),
+                 ErrorMessageResourceName = "NameRequired")]
+        [Display(Name = "Name", ResourceType = typeof(Resources.Resource))]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Resources.Resource),
+               ErrorMessageResourceName = "CityRequired")]
+        [Display(Name = "City", ResourceType = typeof(Resources.Resource))]
+        public string? City { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Resources.Resource),
+                ErrorMessageResourceName = "CountryRequired")]
+        [Display(Name = "Country", ResourceType = typeof(Resources.Resource))]
+        public string? Country { get; set; }
+    }
+}
