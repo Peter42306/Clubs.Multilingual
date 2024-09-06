c779e8c0ddd93d3e4f0a48a67df671b523693f0d
Petr Zalizko <pzalizko@gmail.com>
2024-09-06 17:28:17 +0300

    Add project files.

---
+++ Clubs.Multilingual/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Clubs.Multilingual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Clubs.Multilingual.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Clubs.Multilingual/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clubs.Multilingual.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
